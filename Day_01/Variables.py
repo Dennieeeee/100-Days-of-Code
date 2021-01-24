c990227a9ace79e62553f8d0a3d617ab20af160a
Dennieeeee <38169907+Dennieeeee@users.noreply.github.com>
2021-01-23 16:50:13 -0800

    Add files via upload

---
+++ Day_01/Variables.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#### switch the input of a and b; so whatever the input is in a will goes to b, vice versa
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
---
+++ Day_01/main.py
@@ -0,0 +1,4 @@
+name = input('Enter your name here: \n')
+name_len = len(name)
+print(f'Your name has {name_len} characters.')
+
